c3fff9aea74abdbe428687b5ddec1e1233670a58
alireza <farmanialireza668@gmail.com>
2025-04-18 18:43:55 +0330

    install get

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -14,7 +14,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   int _index = 0;
-  List<Widget> _pages = [HomeScreen() ,ExpelorScreen() ];
+  final List<Widget> _pages = [HomeScreen() ,ExpelorScreen() ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
